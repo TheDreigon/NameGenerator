abadabeb74affce835ea06c68de473f2b79ed5b2
thedreigon <miguelangelo_fm@hotmail.com>
2021-05-09 01:14:06 +0100

    first commit

---
+++ src/test/java/com/github/TheDreigon/NameGenerator/NameGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.TheDreigon.NameGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
